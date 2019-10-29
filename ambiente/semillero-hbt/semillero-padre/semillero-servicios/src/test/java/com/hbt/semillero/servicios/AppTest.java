b73d7f9e3f851e6d6e2290a626082d9335008b5c
Angyetv <angi-danitv@hotmail.com>
2019-10-29 08:40:56 -0500

    Ejercicio 1 Asserts.
    Probar el método invertir cadena.

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,47 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
+	}
+
+	@Test
+	public void primeraPU() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L;
+		Long sumando2 = 50L;
+		Long resultado = sumando1 + sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+		resultadoEsperado = 200L;
+		Assert.assertNotEquals(resultado, resultadoEsperado);
+	}
+
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+		return cadenaInvertida;
+	}
+	
+	/**
+	 * Método para validar el funcionamiento de invertir cadena.
+	 */
+	@Test
+	public void probarInvierteCadena() {
+		String cadena = "El hijo de rana, Rinrín renacuajo salió esta mañana muy tieso, muy majo.";
+		String resultado = ".ojam yum ,oseit yum anañam atse óilas ojaucaner nírniR ,anar ed ojih lE";
+		Assert.assertEquals(invertirCadena(cadena), resultado); // Validado con una cadena larga que tiene espacios.
+		resultado = ".ojam yum,oseit yum anañam atse óilas ojaucaner nírniR ,anar ed ojih lE";
+		Assert.assertNotEquals(invertirCadena(cadena), resultado); // Validado con una cadena que tiene un espacio de diferencia.
+		cadena = " ";
+		resultado = " ";
+		Assert.assertEquals(invertirCadena(cadena), resultado); // Validado con dos cadenas con espacio.
+		resultado = "";
+		Assert.assertNotEquals(invertirCadena(cadena), resultado); //Validado con una cadena con espacio y otra vacia.
 	}
 }
