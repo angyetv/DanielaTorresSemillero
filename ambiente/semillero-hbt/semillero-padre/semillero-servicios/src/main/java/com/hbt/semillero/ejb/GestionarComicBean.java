bc7074a32e241cf97a803af0a577ac5867eea0d3
Angyetv <angi-danitv@hotmail.com>
2019-11-01 09:41:44 -0500

    .

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/ejb/GestionarComicBean.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/ejb/GestionarComicBean.java
@@ -1,1 +1,63 @@
+/**
  * GestionarComicBean.java
+ */
+package com.hbt.semillero.ejb;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.hbt.semillero.dto.ComicDTO;
+import com.hbt.semillero.entidades.Comic;
+
+/**
+ * <b>Descripción:<b> Clase que determina <b>Caso de Uso:<b>
+ * 
+ * @author Angye Daniela Torres.
+ * @version
+ */
+@Stateless
+public class GestionarComicBean {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	// TODO
+	/*
+	 * agregar interfaz
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void crearComicDTO(ComicDTO comicNuevo) {
+		// Entidad nueva
+		Comic comic = new Comic();
+		comic.setId(comicNuevo.getId());
+		// TODO llenar los demas datos
+		em.persist(comicNuevo);
+	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void modificarComic(ComicDTO comicModificar) {
+		Comic comic = new Comic();
+		comic.setId(comicModificar.getId());
+		// TODO llenar los demas datos
+		em.merge(comic);
+	}
+
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public ComicDTO consultarComic(String idComic) {
+		Comic comic = em.find(Comic.class, idComic);
+		ComicDTO comicDTO = new ComicDTO(comic.getId(), comic.getNombre());
+		// TODO llenar los demas datos
+		return comicDTO;
+	}
+
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public List<ComicDTO> consultarComic() {
+		List<Comic> resultados = (List<Comic>) em.createQuery("select c from Comic").getResultList();
+		return null;
+	}
+}
